c8807268f1a018a16e3e99ce3c603f97e7f30041
Harris <harrisdt15f@gmail.com>
2019-12-15 19:39:38 +0800

    :sparkles: t

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -6,8 +6,7 @@ use EloquentFilter\Filterable;
 use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Model;
 
-class BaseModel extends Model
-{
+class BaseModel extends Model{
     use Cachable, Filterable;
 
     private const DEFAULT_PAGESIZE = 50;
