872bfa54df05062167022e0c87fc7b2fadf547dc
Harris <harrisdt15f@gmail.com>
2020-01-11 18:45:15 +0800

    :sparkles: rule of ssl

---
+++ config/ssl.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    //生成ssl规则
+    'rule' => [
+        'digest_alg'       => 'sha1024',
+        'private_key_bits' => 1024,                //字节数  512 1024 2048  4096 等
+        'private_key_type' => OPENSSL_KEYTYPE_RSA, //加密类型
+    ],
+];
