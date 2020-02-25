b88a141a3a8955f795d476ea518b1dc5f429e26d
Harris <harrisdt15f@gmail.com>
2020-02-25 21:18:59 +0800

    ✨ update BaseAuthModel.php

--- app/Models/BaseAuthModel.php
+++ app/Models/BaseAuthModel.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace App\Models;
 
+use App\Models\Logics\BaseModelLogics;
+use EloquentFilter\Filterable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use EloquentFilter\Filterable;
-use App\Models\Traits\BaseModelLogics;
 
 /**
  * Class for base auth model.
  */
 class BaseAuthModel extends Authenticatable implements JWTSubject
 {
-    use Filterable,BaseModelLogics;
+    use Filterable;
+    use BaseModelLogics;
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
@@ -20,16 +22,18 @@ class BaseAuthModel extends Authenticatable implements JWTSubject
      */
     public function getJWTIdentifier()
     {
-        return $this->getKey();
+        $jwtKey = $this->getKey();
+        return $jwtKey;
     }
 
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
      *
-     * @return array
+     * @return mixed[]
      */
-    public function getJWTCustomClaims()
+    public function getJWTCustomClaims(): array
     {
-        return [];
+        $claim = [];
+        return $claim;
     }
 }
