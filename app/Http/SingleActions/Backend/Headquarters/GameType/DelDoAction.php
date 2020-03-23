52f6e0ebe3055af16e14ba183f8fcfa59db6d47c
ethan <ethan.kekeke@gmail.com>
2020-03-23 22:48:47 +0800

    delete game

--- app/Http/SingleActions/Backend/Headquarters/GameType/DelDoAction.php
+++ app/Http/SingleActions/Backend/Headquarters/GameType/DelDoAction.php
@@ -3,7 +3,6 @@
 namespace App\Http\SingleActions\Backend\Headquarters\GameType;
 
 use App\Http\Requests\Backend\Headquarters\GameType\DelDoRequest;
-use App\Models\Game\Game;
 use Illuminate\Http\JsonResponse;
 use Log;
 
@@ -17,16 +16,21 @@ class DelDoAction
     /**
      * @param DelDoRequest $request DelDoRequest.
      * @return JsonResponse
-     * @throws \RuntimeException RuntimeException.
+     * @throws \RuntimeException Exception.
      */
     public function execute(DelDoRequest $request): JsonResponse
     {
+        $validated     = $request->validated();
+        $model         = $request->get('model');// 从 App\Rules\Backend\Common\Sortable\CheckSortableModel 注入
+        $categoryModel = $model::where('id', $validated['id'])->first();
+
+        if (!$categoryModel) {
+            throw new \RuntimeException('300405');
+        }
+        if ($categoryModel->games()->exists()) {
+            throw new \RuntimeException('300400');
+        }
         try {
-            $validated = $request->validated();
-            $model     = $request->get('model');// 从 App\Rules\Backend\Common\Sortable\CheckSortableModel 注入
-            if (!Game::where('vendor_id', $validated['id'])->get()->isEmpty()) {
-                throw new \RuntimeException('300400');
-            }
             $model::find($validated['id'])->delete();
             $msgOut = msgOut();
             return $msgOut;
