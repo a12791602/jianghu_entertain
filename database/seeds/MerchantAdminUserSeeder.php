dcf522fa0a7896ccb42d7a77abba5465dc0d2222
alvin <953909858@qq.com>
2020-03-27 17:12:09 +0800

    seed数据修改

--- database/seeds/MerchantAdminUserSeeder.php
+++ database/seeds/MerchantAdminUserSeeder.php
@@ -19,9 +19,9 @@ class MerchantAdminUserSeeder extends Seeder
             [
              [
               'id'              => '1',
-              'name'            => 'ling',
-              'email'           => 'ling@gmail.com',
-              'password'        => '$2y$10$MKKGeBpoDfzfcewJ4Mrk.uTCIcSSvowXwPeKw0LNwY8K60pz7QL3G',
+              'name'            => 'Harris',
+              'email'           => 'harrisdt15f@gmail.com',
+              'password'        => '$2y$10$Z1XyP6L7AclsM3sG6KxRhOtqHmLi2cyhbrKiNmJjK7rBjlv.rWCEe',
               'remember_token'  => '',
               'group_id'        => '1',
               'status'          => '1',
@@ -31,6 +31,18 @@ class MerchantAdminUserSeeder extends Seeder
              ],
              [
               'id'              => '2',
+              'name'            => 'ling',
+              'email'           => 'ling@gmail.com',
+              'password'        => '$2y$10$Z1XyP6L7AclsM3sG6KxRhOtqHmLi2cyhbrKiNmJjK7rBjlv.rWCEe',
+              'remember_token'  => '',
+              'group_id'        => '2',
+              'status'          => '1',
+              'platform_id'     => 1,
+              'platform_sign'   => 'jhhy',
+              'chargeable_fund' => 100,
+             ],
+             [
+              'id'              => '3',
               'name'            => 'Charon',
               'email'           => 'ezreal0520@gmail.com',
               'password'        => '$2y$10$OjyyasS6GMcRoD4cwASRzOchhbGRRUjoXduWIwSj3MZ4gLGeT9S6W',
@@ -42,7 +54,7 @@ class MerchantAdminUserSeeder extends Seeder
               'chargeable_fund' => 100,
              ],
              [
-              'id'              => '3',
+              'id'              => '4',
               'name'            => 'aaron',
               'email'           => 'aaron@qq.com',
               'password'        => '$2y$10$hKf16yCcHI3gaPT06DVOle.uRM.dOugg6zt4A91qXDptxnGZEjOqi',
