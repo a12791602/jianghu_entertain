62f119ecdf4ca5128552808b8b3b8939465fcd39
alvin <953909858@qq.com>
2020-04-03 15:34:39 +0800

    菜单&路由seed整理

--- database/seeds/BackendSystemMenuSeeder.php
+++ database/seeds/BackendSystemMenuSeeder.php
@@ -20,7 +20,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '1',
               'label'   => '首页',
-              'en_name' => '',
+              'en_name' => 'home',
               'route'   => '/home',
               'pid'     => 0,
               'icon'    => 'iconhome',
@@ -32,8 +32,8 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '2',
               'label'   => '首页',
-              'en_name' => '',
-              'route'   => '/home',
+              'en_name' => 'Home',
+              'route'   => '/home/home',
               'pid'     => 1,
               'icon'    => null,
               'display' => 1,
@@ -44,7 +44,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '3',
               'label'   => '厅主管理',
-              'en_name' => '',
+              'en_name' => 'hall',
               'route'   => '/hall',
               'pid'     => 0,
               'icon'    => 'iconaccount',
@@ -56,7 +56,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '4',
               'label'   => '厅主列表',
-              'en_name' => '',
+              'en_name' => 'HallList',
               'route'   => '/hall/halllist',
               'pid'     => 3,
               'icon'    => null,
@@ -68,7 +68,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '5',
               'label'   => '登录记录',
-              'en_name' => '',
+              'en_name' => 'LoginRecord',
               'route'   => '/hall/loginrecord',
               'pid'     => 3,
               'icon'    => null,
@@ -81,7 +81,7 @@ class BackendSystemMenuSeeder extends Seeder
               'id'      => '6',
               'label'   => '游戏管理',
               'en_name' => '',
-              'route'   => '/home',
+              'route'   => '',
               'pid'     => 0,
               'icon'    => 'icongame',
               'display' => 1,
@@ -92,7 +92,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '7',
               'label'   => '厂商管理',
-              'en_name' => '',
+              'en_name' => 'VendorManage',
               'route'   => '/game/vendormanage',
               'pid'     => 6,
               'icon'    => null,
@@ -104,7 +104,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '8',
               'label'   => '分类设置',
-              'en_name' => '',
+              'en_name' => 'SortSet',
               'route'   => '/game/sortset',
               'pid'     => 6,
               'icon'    => null,
@@ -116,7 +116,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '9',
               'label'   => '游戏管理',
-              'en_name' => '',
+              'en_name' => 'GameManage',
               'route'   => '/game/gamemanage',
               'pid'     => 6,
               'icon'    => 'iconhuodong',
@@ -128,8 +128,8 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '10',
               'label'   => '活动管理',
-              'en_name' => '',
-              'route'   => '/active',
+              'en_name' => 'active',
+              'route'   => '',
               'pid'     => 0,
               'icon'    => null,
               'display' => 1,
@@ -140,7 +140,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '11',
               'label'   => '活动列表',
-              'en_name' => '',
+              'en_name' => 'ActiveList',
               'route'   => '/active/activelist',
               'pid'     => 10,
               'icon'    => null,
@@ -152,7 +152,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '12',
               'label'   => '邮件系统',
-              'en_name' => '',
+              'en_name' => 'email',
               'route'   => '/email',
               'pid'     => 0,
               'icon'    => 'icon185078emailmailstreamline',
@@ -164,7 +164,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '13',
               'label'   => '发邮件',
-              'en_name' => '',
+              'en_name' => 'SendEmail',
               'route'   => '/email/sendemail',
               'pid'     => 12,
               'icon'    => null,
@@ -176,7 +176,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '14',
               'label'   => '收件箱',
-              'en_name' => '',
+              'en_name' => 'ReceiveEmail',
               'route'   => '/email/receiveemail',
               'pid'     => 12,
               'icon'    => null,
@@ -188,7 +188,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '15',
               'label'   => '已发邮件',
-              'en_name' => '',
+              'en_name' => 'EmailSent',
               'route'   => '/email/emailsent',
               'pid'     => 12,
               'icon'    => null,
@@ -200,8 +200,8 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '16',
               'label'   => '报表管理',
-              'en_name' => '',
-              'route'   => '/report',
+              'en_name' => 'report',
+              'route'   => '',
               'pid'     => 0,
               'icon'    => 'iconassistant_lefticon_Statisticalreportforms',
               'display' => 1,
@@ -212,7 +212,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '17',
               'label'   => '厅主注单报表',
-              'en_name' => '',
+              'en_name' => 'HallRegist',
               'route'   => '/report/hallregist',
               'pid'     => 16,
               'icon'    => null,
@@ -224,7 +224,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '18',
               'label'   => '第三方游戏报表',
-              'en_name' => '',
+              'en_name' => 'ThirdGame',
               'route'   => '/report/thirdgame',
               'pid'     => 16,
               'icon'    => null,
@@ -236,7 +236,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '19',
               'label'   => '厅主游戏报表',
-              'en_name' => '',
+              'en_name' => 'HallGame',
               'route'   => '/report/hallgame',
               'pid'     => 16,
               'icon'    => null,
@@ -248,7 +248,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '20',
               'label'   => '厅主充提报表',
-              'en_name' => '',
+              'en_name' => 'HallDeposit',
               'route'   => '/report/halldeposit',
               'pid'     => 16,
               'icon'    => null,
@@ -260,8 +260,8 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '21',
               'label'   => '金流配置',
-              'en_name' => '',
-              'route'   => '/cashflow',
+              'en_name' => 'cashflow',
+              'route'   => '',
               'pid'     => 0,
               'icon'    => 'iconmoneybag',
               'display' => 1,
@@ -272,7 +272,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '22',
               'label'   => '厂商管理',
-              'en_name' => '',
+              'en_name' => 'CashFlowVendor',
               'route'   => '/cashflow/cashflowvendor',
               'pid'     => 21,
               'icon'    => null,
@@ -284,7 +284,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '23',
               'label'   => '分类管理',
-              'en_name' => '',
+              'en_name' => 'CashFlowSort',
               'route'   => '/cashflow/cashflowsort',
               'pid'     => 21,
               'icon'    => null,
@@ -296,7 +296,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '24',
               'label'   => '通道管理',
-              'en_name' => '',
+              'en_name' => 'ChannelManage',
               'route'   => '/cashflow/channelmanage',
               'pid'     => 21,
               'icon'    => null,
@@ -308,8 +308,8 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '25',
               'label'   => '设置管理',
-              'en_name' => '',
-              'route'   => '/set',
+              'en_name' => 'set',
+              'route'   => '',
               'pid'     => 0,
               'icon'    => 'iconshezhi2',
               'display' => 1,
@@ -320,7 +320,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '26',
               'label'   => '管理员分组',
-              'en_name' => '',
+              'en_name' => 'AdminSort',
               'route'   => '/set/adminsort',
               'pid'     => 25,
               'icon'    => null,
@@ -332,7 +332,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '27',
               'label'   => '操作日志',
-              'en_name' => '',
+              'en_name' => 'OperatLog',
               'route'   => '/set/operatlog',
               'pid'     => 25,
               'icon'    => null,
@@ -344,7 +344,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '28',
               'label'   => '短信配置',
-              'en_name' => '',
+              'en_name' => 'SmsConfig',
               'route'   => '/set/smsconfig',
               'pid'     => 25,
               'icon'    => null,
@@ -356,8 +356,8 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '29',
               'label'   => '开发管理',
-              'en_name' => '',
-              'route'   => '/dev',
+              'en_name' => 'dev',
+              'route'   => '',
               'pid'     => 0,
               'icon'    => 'iconkaifa',
               'display' => 1,
@@ -368,7 +368,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '30',
               'label'   => '总控菜单管理',
-              'en_name' => '',
+              'en_name' => 'TotalMenu',
               'route'   => '/dev/totalmenu',
               'pid'     => 29,
               'icon'    => null,
@@ -380,7 +380,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '31',
               'label'   => '厅主菜单管理',
-              'en_name' => '',
+              'en_name' => 'HallMenu',
               'route'   => '/dev/hallmenu',
               'pid'     => 29,
               'icon'    => null,
@@ -392,7 +392,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '33',
               'label'   => '游戏分类配置',
-              'en_name' => '',
+              'en_name' => 'GameSort',
               'route'   => '/dev/gamesort',
               'pid'     => 29,
               'icon'    => null,
@@ -404,7 +404,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '34',
               'label'   => '游戏管理配置',
-              'en_name' => '',
+              'en_name' => 'GameManaConfig',
               'route'   => '/dev/gamemanaconfig',
               'pid'     => 29,
               'icon'    => null,
@@ -416,7 +416,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '35',
               'label'   => '金流厂商配置',
-              'en_name' => '',
+              'en_name' => 'CashVendorConfig',
               'route'   => '/dev/cashvendorconfig',
               'pid'     => 29,
               'icon'    => null,
@@ -428,7 +428,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '36',
               'label'   => '金流分类管理',
-              'en_name' => '',
+              'en_name' => 'CashSort',
               'route'   => '/dev/cashsort',
               'pid'     => 29,
               'icon'    => null,
@@ -440,7 +440,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '37',
               'label'   => '金流通道配置',
-              'en_name' => '',
+              'en_name' => 'CashChannel',
               'route'   => '/dev/cashchannel',
               'pid'     => 29,
               'icon'    => null,
@@ -452,7 +452,7 @@ class BackendSystemMenuSeeder extends Seeder
              [
               'id'      => '38',
               'label'   => '金流银行卡配置',
-              'en_name' => '',
+              'en_name' => 'CashBankCard',
               'route'   => '/dev/cashbankcard',
               'pid'     => 29,
               'icon'    => null,
