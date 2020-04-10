160293e6d1d31c53b759569f92bb8de341e21e6f
Harris <harrisdt15f@gmail.com>
2020-04-10 23:22:44 +0800

    添加 三方 游戏投注订单 记录

--- app/Models/Game/GameProject.php
+++ app/Models/Game/GameProject.php
@@ -3,6 +3,7 @@
 namespace App\Models\Game;
 
 use App\Models\BaseModel;
+use App\Models\User\Logics\GameProjectLogics;
 
 /**
  * Class Game
@@ -10,6 +11,14 @@ use App\Models\BaseModel;
  */
 class GameProject extends BaseModel
 {
+    use GameProjectLogics;
+
+
+    public const STATUS_BET           = 0;//0已投注
+    public const STATUS_CANCEL        = 1;//1已撤销
+    public const STATUS_LOSE          = 2;//2未中奖
+    public const STATUS_WIN           = 3;//3已中奖
+    public const STATUS_WIN_CALCULATE = 4;//4已派奖
 
     /**
      * @var array
