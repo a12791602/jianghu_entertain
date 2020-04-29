18fad016b8dab64e44691c3fb610e7d82841873e
ethan <ethan.kekeke@gmail.com>
2020-04-29 20:55:40 +0800

    update seeds and migration

--- database/seeds/SystemFePageBannerSeeder.php
+++ database/seeds/SystemFePageBannerSeeder.php
@@ -19,7 +19,7 @@ class SystemFePageBannerSeeder extends Seeder
             [
              [
               'title'        => '无限极代理',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/5d446820bdac52ae0c6c4948b6c79a23.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/f633ae459e72b655971c09a593e9f7e4.png',
               'type'         => 1,
               'redirect_url' => 'promote',
               'status'       => 1,
@@ -27,7 +27,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '不定时幸运',
-              'pic_path'     => 'uploads/test/slides/2020-01-09/6e99f27b02c88c913e64d799f7891c2d.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/705d33f291acc33ab667044d8cc061cc.png',
               'type'         => 1,
               'redirect_url' => 'promote',
               'status'       => 1,
@@ -35,7 +35,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '未赌先赢',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/f01db873b8e9fafcd77b9a1ed802c02f.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/f0fafe04c748200b8aeb0da91fc75810.png',
               'type'         => 2,
               'redirect_url' => 'https://www.baidu.com',
               'status'       => 1,
@@ -43,7 +43,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '新人升级有礼',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/fb45df5a9607c475981310d56fda5f2a.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/8e32e3725252f8b50451631e32e8d88a.png',
               'type'         => 2,
               'redirect_url' => 'https://www.baidu.com',
               'status'       => 1,
@@ -51,7 +51,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/5d446820bdac52ae0c6c4948b6c79a23.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/f633ae459e72b655971c09a593e9f7e4.png',
               'type'         => 1,
               'redirect_url' => 'promote',
               'status'       => 1,
@@ -59,7 +59,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/5d446820bdac52ae0c6c4948b6c79a23.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/f633ae459e72b655971c09a593e9f7e4.png',
               'type'         => 1,
               'redirect_url' => 'login',
               'status'       => 1,
@@ -67,7 +67,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/5d446820bdac52ae0c6c4948b6c79a23.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/f633ae459e72b655971c09a593e9f7e4.png',
               'type'         => 1,
               'redirect_url' => 'register',
               'status'       => 1,
@@ -75,7 +75,7 @@ class SystemFePageBannerSeeder extends Seeder
              ],
              [
               'title'        => '',
-              'pic_path'     => 'uploads/test/slides/2020-01-08/5d446820bdac52ae0c6c4948b6c79a23.png',
+              'pic_path'     => 'uploads/jhhy/pic/slide/2020-04-23/f633ae459e72b655971c09a593e9f7e4.png',
               'type'         => 1,
               'redirect_url' => 'forget-password',
               'status'       => 1,
