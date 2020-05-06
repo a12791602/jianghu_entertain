3cc3dae9ffbd9417c6b7226cc8ab4d78e67bf185
ethan <ethan.kekeke@gmail.com>
2020-05-06 22:19:40 +0800

    update game vendor field value to allow null

--- app/Http/Requests/Backend/Headquarters/Game/GameVendor/AddDoRequest.php
+++ app/Http/Requests/Backend/Headquarters/Game/GameVendor/AddDoRequest.php
@@ -22,23 +22,23 @@ class AddDoRequest extends BaseFormRequest
      * @var array 自定义字段 【此字段在数据库中没有的字段字典】
      */
     protected $extraDefinition = [
-                                  'whitelist_ips.*'             => '白名单',
-                                  'production.app_id'           => '终端号',
-                                  'production.des_key'          => 'des 秘钥',
-                                  'production.md5_key'          => 'md5 密钥',
-                                  'production.merchant_id'      => '商户号',
-                                  'production.public_key'       => '公钥',
-                                  'production.private_key'      => '私钥',
-                                  'production.merchant_secret'  => '商户密钥',
-                                  'production.third_party_urls' => '三方调用测试urls',
-                                  'staging.app_id'              => '终端号',
-                                  'staging.des_key'             => 'des 秘钥',
-                                  'staging.md5_key'             => 'md5 密钥',
-                                  'staging.merchant_id'         => '商户号',
-                                  'staging.public_key'          => '公钥',
-                                  'staging.private_key'         => '私钥',
-                                  'staging.merchant_secret'     => '商户密钥',
-                                  'staging.third_party_urls'    => '三方调用测试urls',
+                                  'whitelist_ips.*'            => '白名单',
+                                  'production.app_id'          => '终端号',
+                                  'production.des_key'         => 'des 秘钥',
+                                  'production.md5_key'         => 'md5 密钥',
+                                  'production.merchant_id'     => '商户号',
+                                  'production.public_key'      => '公钥',
+                                  'production.private_key'     => '私钥',
+                                  'production.merchant_secret' => '商户密钥',
+                                  'production.third_party_url' => '正式地址',
+                                  'staging.app_id'             => '终端号',
+                                  'staging.des_key'            => 'des 秘钥',
+                                  'staging.md5_key'            => 'md5 密钥',
+                                  'staging.merchant_id'        => '商户号',
+                                  'staging.public_key'         => '公钥',
+                                  'staging.private_key'        => '私钥',
+                                  'staging.merchant_secret'    => '商户密钥',
+                                  'staging.third_party_url'    => '测试三方url',
                                  ];
 
     /**
@@ -59,33 +59,33 @@ class AddDoRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-                'name'                          => 'required|unique:game_vendors,name',
-                'sign'                          => 'required|string|unique:game_vendors,sign|max:6',
-                'whitelist_ips'                 => 'required|array',
-                'whitelist_ips.*'               => 'ip',
-                'authorization_code'            => 'string|max:10',
-                'status'                        => 'required|in:0,1',
-                'type_id'                       => 'required|integer|exists:game_types,id',
-                'production'                    => 'array',
-                'production.app_id'             => 'string|max:128',
-                'production.merchant_id'        => 'string|max:10',
-                'production.public_key'         => 'required_without_all:production.merchant_secret,production.md5_key|string|max:2048',
-                'production.private_key'        => 'required_without_all:production.merchant_secret,production.md5_key|string|max:2048',
-                'production.merchant_secret'    => 'required_without_all:production.public_key,production.private_key,production.md5_key|string|max:128',
-                'production.md5_key'            => 'required_without_all:production.public_key,production.private_key,production.merchant_secret|string|max:32',
-                'production.des_key'            => 'string|max:64',
-                'production.url.*'              => 'url',
-                'production.third_party_urls.*' => 'url',
-                'staging'                       => 'array',
-                'staging.app_id'                => 'string|max:128',
-                'staging.merchant_id'           => 'string|max:10',
-                'staging.public_key'            => 'string|max:2048',
-                'staging.private_key'           => 'string|max:2048',
-                'staging.merchant_secret'       => 'string|max:128',
-                'staging.md5_key'               => 'string|max:32',
-                'staging.des_key'               => 'string|max:64',
-                'staging.url.*'                 => 'url',
-                'staging.third_party_urls.*'    => 'url',
+                'name'                       => 'required|unique:game_vendors,name',
+                'sign'                       => 'required|string|unique:game_vendors,sign|max:6',
+                'whitelist_ips'              => 'required|array',
+                'whitelist_ips.*'            => 'ip',
+                'authorization_code'         => 'string|max:10',
+                'status'                     => 'required|in:0,1',
+                'type_id'                    => 'required|integer|exists:game_types,id',
+                'production'                 => 'array',
+                'production.app_id'          => 'string|max:128|nullable',
+                'production.merchant_id'     => 'string|max:10|nullable',
+                'production.public_key'      => 'required_without_all:production.merchant_secret,production.md5_key|string|max:2048|nullable',
+                'production.private_key'     => 'required_without_all:production.merchant_secret,production.md5_key|string|max:2048|nullable',
+                'production.merchant_secret' => 'required_without_all:production.public_key,production.private_key,production.md5_key|string|max:128|nullable',
+                'production.md5_key'         => 'required_without_all:production.public_key,production.private_key,production.merchant_secret|string|max:32|nullable',
+                'production.des_key'         => 'string|max:64|nullable',
+                'production.url.*'           => 'url|nullable',
+                'production.third_party_url' => 'url|nullable',
+                'staging'                    => 'array',
+                'staging.app_id'             => 'string|max:128|nullable',
+                'staging.merchant_id'        => 'string|max:10|nullable',
+                'staging.public_key'         => 'string|max:2048|nullable',
+                'staging.private_key'        => 'string|max:2048|nullable',
+                'staging.merchant_secret'    => 'string|max:128|nullable',
+                'staging.md5_key'            => 'string|max:32|nullable',
+                'staging.des_key'            => 'string|max:64|nullable',
+                'staging.url.*'              => 'url|nullable',
+                'staging.third_party_url'    => 'url|nullable',
                ];
     }
 
@@ -94,10 +94,6 @@ class AddDoRequest extends BaseFormRequest
      */
     public function filters(): array
     {
-        return [
-                'whitelist_ips'               => 'cast:array',
-                'staging.third_party_urls'    => 'cast:array',
-                'production.third_party_urls' => 'cast:array',
-               ];
+        return ['whitelist_ips' => 'cast:array'];
     }
 }
