28ebad6373c78b15109affc212afbf66ca025561
Harris <harrisdt15f@gmail.com>
2020-05-06 22:54:54 +0800

    IM 转账钱包

---
+++ app/Http/SingleActions/Frontend/Common/GamesLobby/InGameTxBalanceAction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\SingleActions\Frontend\Common\GamesLobby;
+
+use App\Http\SingleActions\MainAction;
+use App\JHHYLibs\GameCommons;
+use App\Models\Game\GameVendor;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
+
+/**
+ * Class InGameAction
+ * @package App\Http\SingleActions\Frontend\Common\GamesLobby
+ */
+class InGameTxBalanceAction extends MainAction
+{
+    /**
+     * @param array $inputDatas InputDatas.
+     * @return JsonResponse
+     * @throws \RuntimeException Exception.
+     */
+    public function execute(array $inputDatas): JsonResponse
+    {
+        $criteriaInput   = Arr::only($inputDatas, 'vendor');
+        $curentVendorObj = GameVendor::filter($criteriaInput)->first();
+        if (! $curentVendorObj instanceof GameVendor) {
+            throw new \RuntimeException('100704');//对不起,游戏厂商不存在!
+        }
+        $gameInstance       = GameCommons::gameInit($curentVendorObj);
+        $inputDatas['guid'] = $this->user->guid;
+        $gameInstance->downScore($inputDatas);
+        return msgOut();
+    }
+}
