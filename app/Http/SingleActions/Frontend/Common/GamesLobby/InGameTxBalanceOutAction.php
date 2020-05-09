1b6e7d616bb232f88f4789531088e0268e113c42
Harris <harrisdt15f@gmail.com>
2020-05-09 20:36:06 +0800

    update bug

--- app/Http/SingleActions/Frontend/Common/GamesLobby/InGameTxBalanceOutAction.php
+++ app/Http/SingleActions/Frontend/Common/GamesLobby/InGameTxBalanceOutAction.php
@@ -5,6 +5,7 @@ namespace App\Http\SingleActions\Frontend\Common\GamesLobby;
 use App\Http\SingleActions\MainAction;
 use App\JHHYLibs\GameCommons;
 use App\Models\Game\GameVendor;
+use App\Models\User\FrontendUser;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Arr;
 
@@ -26,7 +27,10 @@ class InGameTxBalanceOutAction extends MainAction
         if (! $curentVendorObj instanceof GameVendor) {
             throw new \RuntimeException('100704');//对不起,游戏厂商不存在!
         }
-        $gameInstance       = GameCommons::gameInit($curentVendorObj);
+        $gameInstance = GameCommons::gameInit($curentVendorObj);
+        if (! $this->user instanceof FrontendUser) {
+            throw new \RuntimeException('100505');//用户不存在
+        }
         $inputDatas['guid'] = $this->user->guid;
         $gameInstance->upScore($inputDatas);
         return msgOut();
