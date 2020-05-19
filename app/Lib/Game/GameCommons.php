29aec5237a9dd0e1fe7870ef917555f4bfaf04f2
Harris <harrisdt15f@gmail.com>
2020-05-19 13:10:18 +0800

    game common move to lib

---
+++ app/Lib/Game/GameCommons.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Lib\Game;
+
+use App\Models\Game\GameVendor;
+use Illuminate\Support\Facades\Config;
+
+/**
+ * Class GameCommons
+ * @package App\JHHYLibs
+ */
+class GameCommons
+{
+
+    /**
+     * @param GameVendor $vendor GameVendor.
+     * @return \Illuminate\Contracts\Foundation\Application|mixed
+     * @throws \Exception Exception.
+     */
+    public static function gameInit(GameVendor $vendor)
+    {
+        $className = Config::get('games_classes.' . $vendor->sign);
+        if ($className === null) {
+            throw new \Exception('100708');//'游戏服务出错!'
+        }
+        $gameClass = resolve($className);
+        $gameClass->setVendor($vendor);
+        return $gameClass;
+    }
+}
