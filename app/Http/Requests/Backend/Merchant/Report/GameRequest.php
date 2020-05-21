d1c972e2acc8b9bb2dd9489a9e637e5bd225e4d7
alvin <953909858@qq.com>
2020-05-21 20:56:51 +0800

    代理后台-游戏报表

---
+++ app/Http/Requests/Backend/Merchant/Report/GameRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests\Backend\Merchant\Report;
+
+use App\Http\Requests\BaseFormRequest;
+use App\Models\Game\GameVendorReportDay;
+
+/**
+ * 游戏报表-列表
+ */
+class GameRequest extends BaseFormRequest
+{
+
+    /**
+     * @var array 需要依赖模型中的字段备注信息
+     */
+    protected $dependentModels = [GameVendorReportDay::class];
+
+    /**
+     * @var array 自定义字段 【此字段在数据库中没有的字段字典】
+     */
+    protected $extraDefinition = ['project_day' => '起止时间'];
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return boolean
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return mixed[]
+     */
+    public function rules(): array
+    {
+        return [
+                'game_vendor_name' => 'string|max:32',          //游戏厂商
+                'project_day'      => 'array|size:2',           //日期
+                'project_day.*'    => 'date|date_format:Y-m-d', //日期
+                'pageSize'         => 'integer|between:1,100',  //每页数据条数
+               ];
+    }
+
+    /**
+     *  Filters to be applied to the input.
+     *
+     * @return mixed[]
+     */
+    public function filters(): array
+    {
+        return ['project_day' => 'cast:array'];
+    }
+}
---
+++ database/seeds/GameReportDaySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Game\GameReportDay;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class GameReportDaySeeder
+ */
+class GameReportDaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        GameReportDay::insert(
+            [
+             [
+              'id'               => 1,
+              'platform_sign'    => 'JHHY',
+              'game_sign'        => '10002',
+              'game_name'        => '射兔神手',
+              'game_vendor_sign' => 'VR',
+              'bet_money'        => 171.0000,
+              'effective_bet'    => 45.0000,
+              'win_money'        => 126.0000,
+              'our_net_win'      => 0,
+              'commission'       => 0,
+              'day'              => '2020-05-08',
+              'created_at'       => '2020-05-21 17:48:13',
+              'updated_at'       => '2020-05-21 17:48:13',
+             ],
+            ],
+        );
+    }
+}
