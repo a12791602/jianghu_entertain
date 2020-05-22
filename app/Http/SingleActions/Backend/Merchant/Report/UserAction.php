2333afff43df1bb67a37026ac7f99494748852e0
alvin <953909858@qq.com>
2020-05-22 22:32:57 +0800

    代理后台-报表管理-个人报表

--- app/Http/SingleActions/Backend/Merchant/Report/UserAction.php
+++ app/Http/SingleActions/Backend/Merchant/Report/UserAction.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\SingleActions\Frontend\Common\FrontendUser;
+namespace App\Http\SingleActions\Backend\Merchant\Report;
 
 use App\Http\SingleActions\MainAction;
 use App\Models\User\UsersReportDay;
@@ -8,28 +8,27 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 /**
- * Class UserReportAction
- * @package App\Http\SingleActions\Common\FrontendUser
+ * 个人报表-列表
  */
-class UserReportAction extends MainAction
+class UserAction extends MainAction
 {
 
     /**
      * @var object
      */
     protected $model;
-    
-     /**
-      * MainAction constructor.
-      * @param UsersReportDay $usersReportDay 用户日报表.
-      * @param Request        $request        Request.
-      * @throws \Exception    Exception.
-      */
+
+    /**
+     * @param UsersReportDay $usersReportDay 游戏日报表Model.
+     * @param Request        $request        Request.
+     * @throws \Exception Exception.
+     */
     public function __construct(UsersReportDay $usersReportDay, Request $request)
     {
         parent::__construct($request);
         $this->model = $usersReportDay;
     }
+
     /**
      * @param array $inputDatas 接收的参数.
      * @return JsonResponse
@@ -40,22 +39,31 @@ class UserReportAction extends MainAction
         if (isset($inputDatas['pageSize'])) {
             $this->model->setPerPage($inputDatas['pageSize']);
         }
-        $inputDatas['guid']          = $this->user->guid;
         $inputDatas['platform_sign'] = $this->currentPlatformEloq->sign;
         $result                      = $this->model
+            ->filter($inputDatas)
             ->select(
                 [
+                 'day',
+                 'mobile',
+                 'guid',
+                 'recharge_sum',
+                 'recharge_num',
+                 'withdraw_sum',
+                 'withdraw_num',
                  'bet_sum',
-                 'effective_bet_sum',
+                 'bet_num',
                  'reduced_sum',
+                 'effective_bet_sum',
                  'commission',
+                 'activity_sum',
                  'game_win_sum',
+                 'real_win_lose',
                  'day',
                 ],
-            )
-            ->filter($inputDatas)
-            ->orderBy('day', 'desc')
-            ->paginate();
+            )->orderBy('day', 'desc')
+            ->paginate()
+            ->toArray();
         return msgOut($result);
     }
 }
