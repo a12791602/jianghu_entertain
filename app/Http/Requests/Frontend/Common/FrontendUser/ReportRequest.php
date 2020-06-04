fa09fe566ce51dd5f0c0d56c177b52da81f78c3c
alvin <953909858@qq.com>
2020-06-04 18:40:31 +0800

    H5 投注记录

--- app/Http/Requests/Frontend/Common/FrontendUser/ReportRequest.php
+++ app/Http/Requests/Frontend/Common/FrontendUser/ReportRequest.php
@@ -28,10 +28,12 @@ class ReportRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-                'type'         => 'required|integer|in:1,2,3',     //类型  1.账变明细  2.充值记录 3.提现记录
-                'created_at'   => 'array|size:2',                 // 账变时间
-                'created_at.*' => 'date|date_format:Y-m-d H:i:s', //账变时间
-                'pageSize'     => 'integer|between:1,100',         //每页数据条数
+                'type'                => 'required|integer|in:1,2,3,4',  //类型  1.账变明细  2.充值记录 3.提现记录 4.投注记录
+                'created_at'          => 'array|size:2',                 //报表生成时间
+                'created_at.*'        => 'date|date_format:Y-m-d H:i:s', //报表生成时间
+                'their_create_time'   => 'array|size:2',                 //投注时间
+                'their_create_time.*' => 'date|date_format:Y-m-d H:i:s', //投注时间
+                'pageSize'            => 'integer|between:1,100',        //每页数据条数
                ];
     }
 
@@ -42,6 +44,9 @@ class ReportRequest extends BaseFormRequest
      */
     public function filters(): array
     {
-        return ['created_at' => 'cast:array'];
+        return [
+                'created_at'        => 'cast:array',
+                'their_create_time' => 'cast:array',
+               ];
     }
 }
