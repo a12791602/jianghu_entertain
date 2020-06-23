e39e445224b0b4752ea0b5b124ef5a03e5b9ce98
ethan <ethan.kekeke@gmail.com>
2020-06-23 23:06:47 +0800

    update security code check

--- app/Http/Requests/Frontend/Common/PasswordChangeRequest.php
+++ app/Http/Requests/Frontend/Common/PasswordChangeRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Frontend\Common;
 
 use App\Http\Requests\BaseFormRequest;
+use App\Models\User\FrontendUser;
 use App\Rules\Frontend\SecurityCodeCheckRule;
 
 /**
@@ -11,6 +12,12 @@ use App\Rules\Frontend\SecurityCodeCheckRule;
  */
 class PasswordChangeRequest extends BaseFormRequest
 {
+
+    /**
+     * @var array 需要依赖模型中的字段备注信息
+     */
+    protected $dependentModels = [FrontendUser::class];
+
     /**
      * Determine if the user is authorized to make this request.
      *
